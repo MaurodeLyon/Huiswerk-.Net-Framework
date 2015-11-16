ab0abb10a37f959686bcd9583a9de6687591b8ac
Mauro de Lyon <maurodelyon@gmail.com>
2015-11-17 00:12:35 +0100

    rekenmachine

---
+++ HelloWorld/Rekenmachine/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Rekenmachine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private double number1;
+        private double number2;
+        private double answer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            number1 = 0;
+            number2 = 0;
+            answer = 0;
+        }
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            Double.TryParse(inputBox1.Text, out number1);
+            Double.TryParse(inputBox2.Text, out number2);
+            answer = number1 + number2;
+            answerBox.Text = answer.ToString();
+        }
+
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            Double.TryParse(inputBox1.Text, out number1);
+            Double.TryParse(inputBox2.Text, out number2);
+            answer = number1 - number2;
+            answerBox.Text = answer.ToString();
+        }
+
+        private void multiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Double.TryParse(inputBox1.Text, out number1);
+            Double.TryParse(inputBox2.Text, out number2);
+            answer = number1 * number2;
+            answerBox.Text = answer.ToString();
+        }
+
+        private void divideButton_Click(object sender, RoutedEventArgs e)
+        {
+            Double.TryParse(inputBox1.Text, out number1);
+            Double.TryParse(inputBox2.Text, out number2);
+            answer = number1 / number2;
+            answerBox.Text = answer.ToString();
+        }
+    }
+}
